25dbe099242f444d0f777b12b3a2369914d99b63
Lucas Henrique Carvalho Pinto <lcarvalho.henrique@gmail.com>
2023-02-28 07:05:15 -0300

    Mostra pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [pokemon, setPokemon] = React.useState({});
+
+  function Carregar() {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPokemon(data);
+      });
+  }
+
+  return pokemon.sprites ? (
+    <div className="container">
+      <div className="row">
+        <div className="col-5"></div>
+        <div className="col-2">
+          {" "}
+          <img src={pokemon.sprites.front_default} />{" "}
+        </div>
+      </div>      
+    </div>
+  ) : (
+    <div>
+      {" "}
+      <button onClick={Carregar}>Carregar</button>{" "}
     </div>
   );
 }
