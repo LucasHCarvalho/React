ba8779086c964e5771d83dfb6fe84937d6d2338b
Lucas Henrique <61765824+LucasHCarvalho@users.noreply.github.com>
2023-03-23 23:25:37 -0300

    login para videos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import './App.css';
-import React from 'react';
+import Video from "./Video";
+import Header from "./Header";
+import Buscar from "./Buscar";
+import { videos } from "./data/Videos";
+import { VideoContext } from "./VideioContext";
+import ListVideo from "./ListVideos";
 
 function App() {
   const [login, setLogin] = React.useState();
@@ -22,24 +26,27 @@ function App() {
   return (
     !logar?
     <div className="d-flex justify-content-center align-items-center w-100 h-100" 
-          style={{backgroundImage: "url(/images/montain.jpg)"}}>
-        <div style={{width:"400px",
-                    height:"600px"}}
-                    className='d-flex justify-content-center align-items-center flex-column'>
-          <p>Login</p>
-          <div><input type="text" onChange={(e) => setLogin(e.target.value)}/></div>
-          <p style={{marginTop: "20px"}}>Senha</p>
-          <div ><input type="text" onChange={(e) => setSenha(e.target.value)}/></div>
-          <button class="btn btn-secondary" style={{marginTop: "20px"}} onClick={Logar}>Logar</button>  
-          <p>{logar}</p>
-      </div>
+        style={{backgroundImage: "url(/images/montain.jpg)"}}>
+      <div style={{width:"400px",
+                  height:"600px"}}
+                  className='d-flex justify-content-center align-items-center flex-column'>
+        <p>Login</p>
+        <div><input type="text" onChange={(e) => setLogin(e.target.value)}/></div>
+        <p style={{marginTop: "20px"}}>Senha</p>
+        <div ><input type="text" onChange={(e) => setSenha(e.target.value)}/></div>
+        <button class="btn btn-secondary" style={{marginTop: "20px"}} onClick={Logar}>Logar</button>  
+        <p>{logar}</p>
+      </div>  
     </div>
     :
-    <div>
-      <div className="d-flex justify-content-center align-items-center w-100 h-100 flex-column">
-        <p>{login}</p>
-        <button class="btn btn-secondary" onClick={Deslogar}>Deslogar</button>
-      </div>
+    <div className="container">
+      <p>{login}</p>
+      <VideoContext.Provider value={videos}>
+        <Header></Header>
+        <Buscar></Buscar>
+        <ListVideo></ListVideo>
+      </VideoContext.Provider>
+      <button class="btn btn-secondary" onClick={Deslogar}>Deslogar</button>
     </div>
   );
 }
